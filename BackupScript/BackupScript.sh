de8e7c04480e870236c7e75852b5c0a7e69cfe4c
Bhushan <patilb847@gmail.com>
2025-05-26 19:49:25 +0000

    included backup scripts with ReadMe.md

--- BackupScript/BackupScript.sh
+++ BackupScript/BackupScript.sh
@@ -1,1 +1,31 @@
-echo "Hello world"
+dir_path=$1
+parent_dir=$(dirname $dir_path)
+dir_name=$(basename $dir_path)
+date=$(date +"%Y%m%d-%H%M")
+file_name="backup-${dir_name}-${date}.tar.gz"
+log_file="/root/bash_scripts/BackupScript/logs/backup_log$(date +"%Y%m%d").log"
+
+
+if [ ! -d $(dirname $log_file) ]
+then
+	mkdir $(dirname $log_file)
+fi
+
+if [ ! -d "$dir_path" ]
+then	
+	echo "[ $(date) ] : Folder $dir_path does not exist" | tee -a $log_file
+	exit
+fi
+
+cd $(dirname $dir_path)
+tar -cf "$file_name" $dir_name
+cd -
+
+if [ -f "${parent_dir}/${file_name}" ]
+then
+	echo "[ $(date) ] : backup of ${dir_path} successfully taken at ${parent_dir}/${file_name}" | tee -a $log_file
+else
+	echo "[ $(date) ] : failed to take backup of ${dir_path}"
+fi
+
+
---
+++ BackupScript/ReadMe.md
@@ -0,0 +1,7 @@
+*Uses
+    this script accepts directory path as input and create backup with tar.gz extension.
+    
+*features
+    script stores log under /logs folder for each day.
+    backup is created with following formate backup-<directoryname>-<date>-<time>.tar.gz
+    
